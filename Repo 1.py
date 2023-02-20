53be883de369c83a2b3fac8f9743b9250225fb7b
llozadae <124606867+llozadae@users.noreply.github.com>
2023-02-20 17:22:56 -0500

    Delete Repo 1.py

--- Repo 1.py
+++
@@ -1,6 +0,0 @@
-myStr = "la primera vez siempre duele"
-
-
-print("1. escribir el codigo en el block de notas que trae por defecto tu ordenaor")
-print("2. Guardarlo en el escritorio o en la carpeta de tu preferecnia")
-print("3. Ejecutarlo en Git Bash")
