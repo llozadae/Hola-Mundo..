11f6863ea61b6fe16e99dc9296192c9f9ea7fe3a
llozadae <124606867+llozadae@users.noreply.github.com>
2023-02-20 17:11:09 -0500

    Add files via upload

---
+++ Repo 1.py
@@ -0,0 +1,6 @@
+myStr = "la primera vez siempre duele"
+
+
+print("1. escribir el codigo en el block de notas que trae por defecto tu ordenaor")
+print("2. Guardarlo en el escritorio o en la carpeta de tu preferecnia")
+print("3. Ejecutarlo en Git Bash")
